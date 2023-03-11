ee73f6f5ccb7136e495e0e16b4ff05b3bf04a1ff
Jan Delgado <jdelgado@gmx.net>
2023-03-11 16:27:37 +0100

    lesson 5

--- docs/05-receive-feedback/README.md
+++ docs/05-receive-feedback/README.md
@@ -1,1 +1,71 @@
-# wird am 11.03.23 freigeschaltet
+# Sensordaten per MQTT versenden
+
+In dieser Übung empfangen wir Daten vom Backend und stellen diese durch 
+ein visuelles Feedback dar.
+
+Jedesmal wenn das Backend eine Messung empfängt, schickt es eine Bestätigung
+(auch Ackknowledge oder ACK genannt) an das sendende Gerät zurück. Das geschieht
+über das MQTT-Topic `iotlabclt23/<deviceID>/ack`. Als Nachricht wird dabei immer
+die Zeichenfolge `ack` übertragen.
+
+## Anpassungen am Code
+
+Damit wir die Daten per MQTT empfangen können, müssen wir das Programm um 
+folgende Funktionen erweitern:
+
+* unser Device muss sich auf das Topic `iotlabclt23/<deviceid>/ack` subscriben
+* wenn eine Nachricht empfangen wird, dann wird ein LED-Statusobjekt initialisiert
+* in der Hauptschleife muss kontinuierlich das LED-Statusobjekt aktualisiert 
+  werden
+
+### Aufgabe 
+
+Die Funktion `mqtt_on_connect` muss erweitert werden, so dass unser Client
+sich beim Topic `iotlabclt23/<deviceid>/ack` subscribed. Erweitere den Code um
+
+```python
+def mqtt_on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker")
+    # Wir möchten Nachrichten vom Backend auf dem Topic ack_topic empfangen
+    client.subscribe(ack_topic)
+```
+
+Die Variable `ack_topic`, die das MQTT-Topic enthält, ist schon weiter oben
+im Code definiert worden.
+
+Die Funktion `mqtt_on_message` wird nun immer dann aufgerufen, wenn eine
+Nachricht über das MQTT empfangen wurde. Dort intialisieren wir unsere Status
+LED mit einem neuen Effekt:
+
+```python
+def mqtt_on_message(client, topic, message):
+    # Diese Methode wird aufgerufen, wenn wir eine Nachricht empfangen
+    print(f"New message on topic {topic}: {message}")
+    # Effekt für Status LED setzen
+    statusLED.fade_off(750)
+```
+
+Die Variable `statusLED`, die die LED darstellt, ist schon weiter oben im Code
+definiert worden. (Die LED wird mit der JLed Bibliothek gesteuert, siehe auch
+https://jandelgado.github.io/jled-circuitpython/)
+
+Zum Schluss müssen wir die Status LED kontinuierlich in der Hauptschleife
+aktualisieren:
+
+```python
+while True:
+    # MQTT message queue pollen
+    mqtt_client.loop()
+    # Status LED aktualisieren
+    statusLED.update()
+...
+```
+
+Die Stellen sind im Code mit **TODO** Kommentaren markiert.
+
+### Test
+
+Wenn alles funktioniert, sollte jede vom Backend empfangene Nachricht mit
+einem Blinken der LED quittiert werden.
+
+
---
+++ docs/05-receive-feedback/code-template.py
@@ -0,0 +1,118 @@
+# Aufgabe 5 - Feedback empfangen und darstellen
+import time
+import ssl
+import socketpool
+import wifi
+import json
+import board
+import busio
+
+from jled import JLed
+from adafruit_bmp280 import Adafruit_BMP280_I2C as BMP280
+from adafruit_minimqtt import adafruit_minimqtt as MQTT
+from TM1637 import TM1637
+
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# BMP280 sensor connected to I²C pins GP16 (SDA) and GP17 (SCL)
+i2c = busio.I2C(board.GP17, board.GP16)
+sensor = BMP280(i2c, 118)
+display = TM1637(board.GP14,board.GP15)
+statusLED = JLed(board.GP2)
+counter = 1
+
+# Auf diesem Topic sendet der Client seine Messungen an das Backend
+# Format: { "temperature": 22.0, "pressure": 1024 , "deviceid": "abcdef"}
+temperature_topic = "iotlabclt23/" + secrets["deviceid"] + "/temperature"
+
+# Auf diesem Topic empfängt der Client Acknwoledgements vom Backend
+ack_topic = "iotlabclt23/" + secrets["deviceid"] + "/ack"
+
+def connect_to_wifi(ssid, password):
+    print(f"Connecting to WIFI {ssid}")
+    wifi.radio.connect(ssid, password)
+    print(f"Connected to {ssid}!")
+
+
+def mqtt_on_connect(client, userdata, flags, rc):
+    # This function will be called when the client is connected successfully to the broker.
+    print("Connected to MQTT broker")
+    # Wir möchten Nachrichten vom Backend auf dem Topic ack_topic empfangen
+    # **TODO**
+
+
+def mqtt_on_disconnect(client, userdata, rc):
+    # This method is called when the client is disconnected
+    print("Disconnected from MQTT broker")
+
+
+def mqtt_on_message(client, topic, message):
+    # Diese Methode wird aufgerufen, wenn wir eine Nachricht empfangen
+    print(f"New message on topic {topic}: {message}")
+    # Effekt für Status LED setzen
+    # **TODO**
+
+def connect_to_mqtt_broker(broker, port):
+    # Set up a MQTT Client
+    mqtt_client = MQTT.MQTT(
+        broker=broker,
+        port=port,
+        username="",
+        password="",
+        socket_pool=socketpool.SocketPool(wifi.radio),
+        ssl_context=ssl.create_default_context(),
+    )
+
+    # Setup the callback methods above
+    mqtt_client.on_connect = mqtt_on_connect
+    mqtt_client.on_disconnect = mqtt_on_disconnect
+    mqtt_client.on_message = mqtt_on_message
+
+    # Connect the client to the MQTT broker.
+    print(f"Connecting to mqtt://{broker}:{port}...")
+    mqtt_client.connect()
+    return mqtt_client
+
+def sensor_lesen(sensor):
+    """temperatur und druck vom sensor lesen und als tupel zurückgeben"""
+    return sensor.temperature, sensor.pressure
+
+
+def daten_ausgeben(display, counter, temperature, pressure):
+    """Daten auf der Konsole und dem Display ausgeben"""
+    print(f"{counter} - {temperature:.2f} °C, {pressure:.2f} hPa")
+    display.temperature(round(temperature))
+
+
+def daten_senden(mqtt_client, counter, temperature, pressure, deviceid):
+    """Daten in JSON-nachricht verpacken und per MQTT versenden"""
+    message  = json.dumps({"temperature": temperature, 
+                           "pressure": pressure, 
+                           "deviceid": deviceid})
+    print(f"{counter} - Sending message: {message} to {temperature_topic}...")
+    mqtt_client.publish(temperature_topic, message)
+
+
+connect_to_wifi(secrets['ssid'], secrets['password'])
+mqtt_client = connect_to_mqtt_broker(secrets['broker'], secrets['port'])
+last = 0
+
+while True:
+    # MQTT message queue pollen
+    mqtt_client.loop()
+    # Status LED aktualisieren
+    # **TODO**
+
+    # alle 5 Sekunden Sensor lesen und Daten senden und ausgeben
+    now = time.monotonic()
+    if now - last > 5:
+        last = time.monotonic()
+        temperature, pressure = sensor_lesen(sensor)
+        daten_ausgeben(display, counter, temperature, pressure)
+        daten_senden(mqtt_client, counter, temperature, pressure, secrets["deviceid"])
+        counter += 1
+
